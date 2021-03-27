ff6bcb55d688a1af838f875ba7422a64a189663e
Hiroya🍣🍵 <heppokofrontend@gmail.com>
2021-03-27 19:05:59 +0900

    :recycle: Changed the way to get the attribute to via the iframe property.

--- src/HTMLIframeMatchHeightElement.ts
+++ src/HTMLIframeMatchHeightElement.ts
@@ -32,7 +32,7 @@ export default class HTMLIframeMatchHeightElement extends HTMLElement {
 
     if (
       !data ||
-      data.whoAmI !== this.src
+      data.whoAmI !== this.iframe.src
     ) {
       return;
     }
@@ -43,45 +43,11 @@ export default class HTMLIframeMatchHeightElement extends HTMLElement {
 
   get host() {
     try {
-      return new URL(this.src).hostname;
+      return new URL(this.iframe.src).hostname;
     } catch {
       return null;
     }
   }
-
-  get src() { return this.iframe.src }
-  set src(path: string) { this.iframe.src = path }
-
-  get srcdoc() { return this.iframe.srcdoc }
-  set srcdoc(srcdoc: string) { this.iframe.srcdoc = srcdoc }
-
-  get name() { return this.iframe.name }
-  set name(name: string) { this.iframe.name = name }
-
-  get sandbox() { return this.iframe.sandbox }
-  // set sandbox(sandbox: DOMTokenList) { this.iframe.sandbox = sandbox }
-
-  get allow() { return this.iframe.allow }
-  set allow(allow: string) { this.iframe.allow = allow }
-
-  get allowFullscreen() { return this.iframe.allowFullscreen }
-  set allowFullscreen(allowFullscreen: boolean) { this.iframe.allowFullscreen = allowFullscreen }
-
-  get width() { return this.iframe.width }
-  set width(width: string) { this.iframe.width = width }
-
-  get height() { return this.iframe.height }
-  set height(height: string) { this.iframe.height = height }
-
-  get referrerPolicy() { return this.iframe.referrerPolicy }
-  set referrerPolicy(referrerPolicy: ReferrerPolicy) { this.iframe.referrerPolicy = referrerPolicy }
-
-  get loading() { return this.iframe.getAttribute('loading') || 'auto' }
-  set loading(loading: string) { this.iframe.setAttribute('loading', loading) }
-
-  get contentDocument() { return this.iframe.contentDocument }
-
-  get contentWindow() { return this.iframe.contentWindow }
 
   connectedCallback() {
     init(this);
